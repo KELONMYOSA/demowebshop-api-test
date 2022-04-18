e85e91dc0a7d2c6dd5832603a3d38f8971a8681e
Vladislav Samoilenko <79825223592@yandex.ru>
2022-04-19 00:51:25 +0300

    added Allure

--- src/test/java/tests/DemoWebShopTests.java
+++ src/test/java/tests/DemoWebShopTests.java
@@ -1,23 +1,39 @@
 package tests;
 
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
 import io.restassured.http.Cookies;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.filters;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.baseURI;
+import static listeners.CustomAllureListener.customAllureTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class DemoWebShopTests {
 
+    @BeforeAll
+    static void beforeAll() {
+        baseURI = "http://demowebshop.tricentis.com";
+        filters(customAllureTemplate());
+    }
+
     @Test
+    @Owner("KELONMYOSA")
+    @DisplayName("Проверка подписки на новости")
+    @Link(value = "Testing URL", url = "http://demowebshop.tricentis.com")
     void subscribeNewsLetterTest() {
         given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .body("email=123%40123.ru")
                 .when()
-                .post("http://demowebshop.tricentis.com/subscribenewsletter")
+                .post("/subscribenewsletter")
                 .then()
                 .statusCode(200)
                 .body("Success", is(true))
@@ -26,13 +42,16 @@ public class DemoWebShopTests {
     }
 
     @Test
+    @Owner("KELONMYOSA")
+    @DisplayName("Проверка корректного добавления двух товаров в корзину")
+    @Link(value = "Testing URL", url = "http://demowebshop.tricentis.com/")
     void addTwoItemsToCartTest() {
         Cookies cookies =
-                given().
-                        contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                         .body("addtocart_31.EnteredQuantity=1")
                         .when()
-                        .post("http://demowebshop.tricentis.com/addproducttocart/details/31/1")
+                        .post("/addproducttocart/details/31/1")
                         .then()
                         .statusCode(200)
                         .body("success", is(true))
@@ -41,8 +60,8 @@ public class DemoWebShopTests {
                         .body("updatetopcartsectionhtml", is("(1)"))
                         .extract().response().getDetailedCookies();
 
-        given().
-                contentType("application/x-www-form-urlencoded; charset=UTF-8")
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
                 .cookie(String.valueOf(cookies))
                 .body("addtocart_43.EnteredQuantity=1")
                 .when()
@@ -52,14 +71,13 @@ public class DemoWebShopTests {
                 .body("success", is(true))
                 .body("message", is("The product has been added to your " +
                         "<a href=\"/cart\">shopping cart</a>"))
-                .body("updatetopcartsectionhtml", is("(2)"))
-                .extract().response().getDetailedCookies();
+                .body("updatetopcartsectionhtml", is("(2)"));
 
         String checkCartHTML =
                 given()
                         .cookie(String.valueOf(cookies))
                         .when()
-                        .get("http://demowebshop.tricentis.com/cart")
+                        .get("/cart")
                         .then()
                         .statusCode(200)
                         .extract().response().asString();
